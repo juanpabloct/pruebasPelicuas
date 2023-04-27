724484781d12b311cfba98fa19a0671f772c87a2
juanpabloct <juanptorres2003@gmail.com>
2023-04-27 15:21:28 -0500

    Configuration tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        loading: {
+          "0%, 100%": {
+            marginTop: "25px",
+            height: "10px"
+          },
+          "50%": {
+            marginTop: "0px",
+            height: "50px"
+          }
+        }
+      },
+      animation: {
+        'loading': 'loading 1s ease-in-out infinite'
+      },
+    },
   },
   plugins: [],
 }
