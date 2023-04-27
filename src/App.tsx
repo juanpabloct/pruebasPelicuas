1a5f826a45836aa5d438d4f1e2c2e2d35c5101fc
juanpabloct <juanptorres2003@gmail.com>
2023-04-27 15:30:14 -0500

    test animation

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,30 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Header } from "./components/navbar/header";
+import { FlexCol } from "./components/tw-components";
+import { AnimacionCarga } from "./components/animacionCarga";
 const Home = lazy(() => import("./pages/home"));
 function App() {
   const [count, setCount] = useState(0);
-
+  const [search, setSearch] = useState("");
   return (
-    <div className="h-full flex ">
-      <Suspense fallback="Cargando">
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </Router>
-      </Suspense>
-    </div>
+    <FlexCol className="">
+      <Router>
+        <Header setSearch={setSearch} search={search} />
+        {search.length > 0 ? (
+          <>
+            <AnimacionCarga />
+          </>
+        ) : (
+          <Suspense fallback="Cargando">
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </Suspense>
+        )}
+      </Router>
+    </FlexCol>
   );
 }
 
