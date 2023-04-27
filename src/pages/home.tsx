8982207688d009d794fa5cd043bcbd76fa36497d
juanpabloct <juanptorres2003@gmail.com>
2023-04-27 15:37:46 -0500

    Structure for Home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,10 @@
+import { FlexRow, Navbar } from "../components/tw-components";
 import { useConnectApi } from "../customHooks/connectApi";
 
 const Home = () => {
   const { data } = useConnectApi("movie/popular");
   console.log(data);
 
-  return <div>HJomeeee</div>;
+  return <div>sdfsdfsdsdf</div>;
 };
 export default Home;
