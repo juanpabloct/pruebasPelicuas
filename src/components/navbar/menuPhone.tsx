fc93bb8edd71ec4d662476b4558594fab13ec02f
juanpabloct <juanptorres2003@gmail.com>
2023-04-28 07:26:36 -0500

    start  dont show

--- src/components/navbar/menuPhone.tsx
+++ src/components/navbar/menuPhone.tsx
@@ -7,7 +7,7 @@ export const MenuPhone = ({ showMenu }: MenuPhoneProps) => {
   return (
     <FlexCol
       className={`bg-[#141a32] right-0 md:hidden h-[100vh] fixed w-36 items-stretch  top-16  text-white justify-start text-center gap-7 pt-2 transition-all delay-400  ${
-        showMenu ? "translate-x-36  invisible" : "visible "
+        !showMenu ? "translate-x-36  invisible" : "visible "
       }`}
     >
       <Routes />
