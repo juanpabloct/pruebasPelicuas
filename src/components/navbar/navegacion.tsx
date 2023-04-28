3a6bd8ecfaa1db26eabffa5696f98924d2e2317b
juanpabloct <juanptorres2003@gmail.com>
2023-04-27 23:22:03 -0500

    Component of Routes

---
+++ src/components/navbar/navegacion.tsx
@@ -0,0 +1,11 @@
+import { StyleLink } from "./StyleLink";
+
+export const Routes = () => {
+  return (
+    <>
+      <StyleLink route={"#"}>Peliculas</StyleLink>
+      <StyleLink route={"#"}>Programas De Televsion </StyleLink>
+      <StyleLink route={"#"}>Personas</StyleLink>
+    </>
+  );
+};
