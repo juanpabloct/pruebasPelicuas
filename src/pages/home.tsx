d661972506ce02c6c824f0a5947677e8ed794d0d
juanpabloct <juanptorres2003@gmail.com>
2023-04-27 23:24:04 -0500

    get date movie Populars

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,23 @@
-import { FlexRow, Navbar } from "../components/tw-components";
+import { AnimacionCarga } from "../components/animacionCarga";
+import { Target } from "../components/target/target";
+import { FlexCol, FlexRow, Navbar } from "../components/tw-components";
 import { useConnectApi } from "../customHooks/connectApi";
+import { StructureMovies } from "../types/StructureMovies";
 
 const Home = () => {
-  const { data } = useConnectApi("movie/popular");
-  console.log(data);
-
-  return <div>sdfsdfsdsdf</div>;
+  const { data, state } = useConnectApi({ recurso: "movie/popular" });
+  if (state.hasData)
+    return (
+      <div className="w-full aspect-square flex">
+        {data.results.map((movie) => {
+          return <Target pelicula={movie} key={movie.id} />;
+        })}
+      </div>
+    );
+  else if (!state.loading && !state.hasData) {
+    return <div>No hay datos</div>;
+  } else {
+    return <AnimacionCarga />;
+  }
 };
 export default Home;
