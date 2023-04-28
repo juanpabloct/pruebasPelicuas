8afb450b5ee278d0de8eab49d2b3741fb6332d98
juanpabloct <juanptorres2003@gmail.com>
2023-04-28 07:31:07 -0500

    Styles generic

--- src/customHooks/connectApi.tsx
+++ src/customHooks/connectApi.tsx
@@ -1,18 +1,17 @@
 import { useCallback, useEffect, useState } from "react";
 import axios from "axios";
 import { Api, ApiKey } from "../config";
-import { StructureMovies } from "../types/StructureMovies";
 interface useConnectApiProps {
   url?: string;
   recurso: string;
 }
-export const useConnectApi = ({ url, recurso }: useConnectApiProps) => {
+export const useConnectApi = <T,>({ url, recurso }: useConnectApiProps) => {
   let urlComplete = `${Api}/${recurso}?api_key=${ApiKey}`;
   if (url) {
     urlComplete += url;
   }
   const initialData = Object.create([]);
-  const [data, setData] = useState<StructureMovies>(initialData);
+  const [data, setData] = useState<T>(initialData);
 
   const [state, setState] = useState({
     loading: false,
@@ -25,14 +24,14 @@ export const useConnectApi = ({ url, recurso }: useConnectApiProps) => {
     try {
       const peticion = await await axios.get(urlComplete);
       setState((current) => ({ ...current, hasData: true, loading: false }));
+
       setData(peticion.data);
     } catch (error) {
-      setState((current) => ({ ...current, state: true }));
       setState((current) => ({
         ...current,
         hasData: false,
         loading: false,
-        error: false,
+        error: true,
       }));
       setData(initialData);
     }
