d4f933ee30aeaa659e20b426468a860df1b3e250
juanpabloct <juanptorres2003@gmail.com>
2023-04-27 23:23:39 -0500

    Custom hook return state for petition and response

--- src/customHooks/connectApi.tsx
+++ src/customHooks/connectApi.tsx
@@ -1,16 +1,46 @@
 import { useCallback, useEffect, useState } from "react";
 import axios from "axios";
 import { Api, ApiKey } from "../config";
-export const useConnectApi = (url: string) => {
-  const [data, setData] = useState([]);
+import { StructureMovies } from "../types/StructureMovies";
+interface useConnectApiProps {
+  url?: string;
+  recurso: string;
+}
+export const useConnectApi = ({ url, recurso }: useConnectApiProps) => {
+  let urlComplete = `${Api}/${recurso}?api_key=${ApiKey}`;
+  if (url) {
+    urlComplete += url;
+  }
+  const initialData = Object.create([]);
+  const [data, setData] = useState<StructureMovies>(initialData);
+
+  const [state, setState] = useState({
+    loading: false,
+    hasData: false,
+    error: false,
+  });
+
   const getData = useCallback(async () => {
-    const peticion = await axios.get(`${Api}/${url}?api_key=${ApiKey}`);
-    setData(peticion.data);
+    setState((current) => ({ ...current, loading: true }));
+    try {
+      const peticion = await await axios.get(urlComplete);
+      setState((current) => ({ ...current, hasData: true, loading: false }));
+      setData(peticion.data);
+    } catch (error) {
+      setState((current) => ({ ...current, state: true }));
+      setState((current) => ({
+        ...current,
+        hasData: false,
+        loading: false,
+        error: false,
+      }));
+      setData(initialData);
+    }
   }, []);
   useEffect(() => {
     return () => {
       getData();
     };
-  }, []);
-  return { data };
+  }, [recurso, url]);
+  return { data, state };
 };
