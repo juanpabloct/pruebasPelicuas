86e7efd439b7491afe23e0e13e26713f1a7dbdbf
juanpabloct <juanptorres2003@gmail.com>
2023-04-28 07:22:44 -0500

    The animation fulll width

--- src/components/animacionCarga.tsx
+++ src/components/animacionCarga.tsx
@@ -1,11 +1,15 @@
+import { FlexCol } from "./tw-components";
+
 export const AnimacionCarga = () => {
   return (
-    <ul className=" loading inline-flex m-0 p-0 list-none gap-[5px]">
-      <li className="w-3 h-3 bg-slate-500 animate-loading "></li>
-      <li className="w-3 h-3 bg-slate-500 animate-loading delay-1s"></li>
-      <li className="w-3 h-3 bg-slate-500 animate-loading delay-300"></li>
-      <li className="w-3 h-3 bg-slate-500 animate-loading delay-500"></li>
-      <li className="w-3 h-3 bg-slate-500 animate-loading delay-700"></li>
-    </ul>
+    <FlexCol className="justify-center items-center">
+      <ul className=" loading inline-flex m-0 p-0 list-none gap-[5px]">
+        <li className="w-3 h-3 bg-slate-500 animate-loading "></li>
+        <li className="w-3 h-3 bg-slate-500 animate-loading delay-1s"></li>
+        <li className="w-3 h-3 bg-slate-500 animate-loading delay-300"></li>
+        <li className="w-3 h-3 bg-slate-500 animate-loading delay-500"></li>
+        <li className="w-3 h-3 bg-slate-500 animate-loading delay-700"></li>
+      </ul>
+    </FlexCol>
   );
 };
