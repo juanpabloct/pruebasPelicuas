7f908101535ad90571ea4f696e9e466c007ebe56
juanpabloct <juanptorres2003@gmail.com>
2023-04-28 07:22:15 -0500

    Add route movie

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,33 @@
 import { Suspense, lazy, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Header } from "./components/navbar/header";
 import { FlexCol } from "./components/tw-components";
 import { AnimacionCarga } from "./components/animacionCarga";
 import { Search } from "./components/search/search";
-import { MenuPhone } from "./components/navbar/menuPhone";
 const Home = lazy(() => import("./pages/home"));
+const Movie = lazy(() => import("./pages/movie"));
 function App() {
-  const [count, setCount] = useState(0);
   const [search, setSearch] = useState("");
 
   return (
-    <Router>
-      <Header setSearch={setSearch} search={search} />
-      {search.length > 0 ? (
-        <>
-          <Search filter={search} />
-        </>
-      ) : (
-        <Suspense fallback={<AnimacionCarga />}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </Suspense>
-      )}
-    </Router>
+    <FlexCol className="min-h-screen">
+      <Router>
+        <Header setSearch={setSearch} search={search} />
+        {search.length > 0 ? (
+          <>
+            <Search filter={search} setSearch={setSearch} />
+          </>
+        ) : (
+          <Suspense fallback={<AnimacionCarga />}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/movie/:id" element={<Movie />} />
+            </Routes>
+          </Suspense>
+        )}
+      </Router>
+    </FlexCol>
   );
 }
 
