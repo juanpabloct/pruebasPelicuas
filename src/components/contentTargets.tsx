bc5fcc3bf82c7279bc338d0eb31bad6d2bdc715b
juanpabloct <juanptorres2003@gmail.com>
2023-04-28 14:15:11 -0500

    ContentTargets

---
+++ src/components/contentTargets.tsx
@@ -0,0 +1,30 @@
+import { Dispatch, SetStateAction } from "react";
+import { StructureMovies } from "../types/StructureMovies";
+import { Target } from "./target/target";
+import { FlexRow } from "./tw-components";
+
+export const ContentTargets = ({
+  data,
+  setSearch,
+}: {
+  data: StructureMovies;
+  setSearch?: Dispatch<SetStateAction<string>>;
+}) => {
+  return (
+    <FlexRow className="justify-center items-center mt-4">
+      <div className="flex w-10/12  flex-wrap container">
+        {data.results.map((item) => (
+          <div
+            className="w-1/6"
+            onClick={() => {
+              setSearch && setSearch("");
+            }}
+            key={item.id}
+          >
+            <Target pelicula={item} />
+          </div>
+        ))}
+      </div>
+    </FlexRow>
+  );
+};
