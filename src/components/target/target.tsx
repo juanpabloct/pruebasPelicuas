a35a23625fcbc31bb0a96da5966aad38ecfdad91
juanpabloct <juanptorres2003@gmail.com>
2023-04-28 07:29:38 -0500

    add Action and animation

--- src/components/target/target.tsx
+++ src/components/target/target.tsx
@@ -1,14 +1,21 @@
+import { useNavigate } from "react-router-dom";
 import { EstructureMovie } from "../../types/estructureMovie";
+import { Image } from "./image";
 
 export const Target = ({ pelicula }: { pelicula: EstructureMovie }) => {
-  const { adult, backdrop_path, title } = pelicula;
-  console.log();
-
+  const { title, poster_path, id } = pelicula;
+  const navigate = useNavigate();
   return (
-    <img
-      src={`https://image.tmdb.org/t/p/w780${backdrop_path}`}
-      alt={title}
-      className="aspect-video w-1/3 "
-    />
+    <figure className="w-full aspect-[2/3] relative">
+      <Image poster_path={poster_path} title={title} />
+      <figcaption
+        onClick={() => {
+          navigate(`/movie/${id}`);
+        }}
+        className="m-auto text-xl text-transparent hover:text-white w-full text-center text- top-0 h-full items-center bg-none transition-colors ease-in hover:bg-[#00000063]  absolute font-semibold flex justify-center cursor-pointer"
+      >
+        <p className="w-10/12 text-ellipsis text-center">{title}</p>
+      </figcaption>
+    </figure>
   );
 };
