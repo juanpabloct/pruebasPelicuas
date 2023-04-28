8112b1747c6c69a77f09f8c955f32c597f869257
juanpabloct <juanptorres2003@gmail.com>
2023-04-28 07:34:02 -0500

    Control for type scenary of the answer

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,19 +1,13 @@
 import { AnimacionCarga } from "../components/animacionCarga";
-import { Target } from "../components/target/target";
-import { FlexCol, FlexRow, Navbar } from "../components/tw-components";
+import { ScrollMovies } from "../components/scrollMovies";
 import { useConnectApi } from "../customHooks/connectApi";
 import { StructureMovies } from "../types/StructureMovies";
 
 const Home = () => {
-  const { data, state } = useConnectApi({ recurso: "movie/popular" });
-  if (state.hasData)
-    return (
-      <div className="w-full aspect-square flex">
-        {data.results.map((movie) => {
-          return <Target pelicula={movie} key={movie.id} />;
-        })}
-      </div>
-    );
+  const { data, state } = useConnectApi<StructureMovies>({
+    recurso: "movie/popular",
+  });
+  if (state.hasData) return <ScrollMovies data={data} title={"Popular"} />;
   else if (!state.loading && !state.hasData) {
     return <div>No hay datos</div>;
   } else {
