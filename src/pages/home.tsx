e61c5b1e8f5e4897446ab9cf389ed6bc1282a60c
juanpabloct <juanptorres2003@gmail.com>
2023-04-28 13:50:30 -0500

    Add others categories component

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,13 +1,26 @@
 import { AnimacionCarga } from "../components/animacionCarga";
+import { Category } from "../components/moviesCategories/category";
 import { ScrollMovies } from "../components/scrollMovies";
+import { FlexCol } from "../components/tw-components";
 import { useConnectApi } from "../customHooks/connectApi";
+import { useGetCategories } from "../customHooks/getCategories";
 import { StructureMovies } from "../types/StructureMovies";
 
 const Home = () => {
   const { data, state } = useConnectApi<StructureMovies>({
     recurso: "movie/popular",
   });
-  if (state.hasData) return <ScrollMovies data={data} title={"Popular"} />;
+  const { genders, hasData } = useGetCategories();
+  if (state.hasData)
+    return (
+      <FlexCol>
+        <ScrollMovies data={data} title={"Popular"} />
+        {hasData.hasData &&
+          genders.genres.map(({ id, name }) => {
+            return <Category id={id} key={id} name={name} />;
+          })}
+      </FlexCol>
+    );
   else if (!state.loading && !state.hasData) {
     return <div>No hay datos</div>;
   } else {
