9ca85535932cc28b106de3adeac4671ab1c158b4
juanpabloct <juanptorres2003@gmail.com>
2023-04-28 07:30:00 -0500

    Change style

--- src/components/tw-components/navbar.tsx
+++ src/components/tw-components/navbar.tsx
@@ -1,4 +1,4 @@
 import tw from "tailwind-styled-components";
 export const Navbar = tw.nav`
-    flex w-full flex-row justify-around h-16 items-center bg-[#141a32] text-[#ffff] sticky top-0
+    flex w-full flex-row justify-around h-16 items-center bg-[#141a32] text-[#ffff] sticky top-0 z-50 border-b border-white
 `;
