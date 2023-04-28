ba3dac4c1170eccaed6dfd1ce2a0fa879753bf1d
juanpabloct <juanptorres2003@gmail.com>
2023-04-28 14:15:45 -0500

    Validacion para tipo scroll o tarjeta

--- src/components/moviesCategories/category.tsx
+++ src/components/moviesCategories/category.tsx
@@ -1,19 +1,25 @@
 import { useConnectApi } from "../../customHooks/connectApi";
 import { StructureMovies } from "../../types/StructureMovies";
+import { ContentTargets } from "../contentTargets";
 import { ScrollMovies } from "../scrollMovies";
 interface CategoryProps {
   id: number | string;
   name: string;
+  type?: "scroll" | "targets";
 }
-export const Category = ({ id, name }: CategoryProps) => {
+export const Category = ({ id, name, type }: CategoryProps) => {
   const { data, state } = useConnectApi<StructureMovies>({
     recurso: "discover/movie",
     url: `&with_genres=${id}/${name}`,
   });
 
-  return state.hasData ? (
-    <ScrollMovies data={data} title={name} id={id} />
-  ) : (
-    <div />
-  );
+  if (state.hasData) {
+    if (!type || type === "scroll") {
+      return <ScrollMovies data={data} title={name} id={id} />;
+    } else {
+      return <ContentTargets data={data} />;
+    }
+  }
+
+  return <div></div>;
 };
