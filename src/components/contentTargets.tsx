afbab42d086dc2b1af0186dae20a1ee6602c29bc
juanpabloct <juanptorres2003@gmail.com>
2023-04-29 20:19:05 -0500

    Corrigio problema de busqueda

--- src/components/contentTargets.tsx
+++ src/components/contentTargets.tsx
@@ -1,7 +1,7 @@
 import { Dispatch, SetStateAction } from "react";
 import { StructureMovies } from "../types/StructureMovies";
 import { Target } from "./target/target";
-import { FlexRow } from "./tw-components";
+import { FlexCol, FlexRow } from "./tw-components";
 
 export const ContentTargets = ({
   data,
@@ -10,21 +10,26 @@ export const ContentTargets = ({
   data: StructureMovies;
   setSearch?: Dispatch<SetStateAction<string>>;
 }) => {
+  const totalPages = data.total_pages;
   return (
-    <FlexRow className="justify-center items-center mt-4">
-      <div className="flex w-10/12  flex-wrap container">
-        {data.results.map((item) => (
-          <div
-            className="w-1/3 md:w-1/6"
-            onClick={() => {
-              setSearch && setSearch("");
-            }}
-            key={item.id}
-          >
-            <Target pelicula={item} />
-          </div>
-        ))}
-      </div>
-    </FlexRow>
+    <FlexCol>
+      <FlexRow className="justify-center items-center mt-4">
+        <div className="flex w-10/12  flex-wrap container">
+          {data.results.map((item) => (
+            <div
+              className="w-1/3 md:w-1/6"
+              onClick={() => {
+                setSearch && setSearch("");
+              }}
+              key={item.id}
+            >
+              <Target pelicula={item} />
+            </div>
+          ))}
+        </div>
+      </FlexRow>
+      {}
+      <FlexRow />
+    </FlexCol>
   );
 };
